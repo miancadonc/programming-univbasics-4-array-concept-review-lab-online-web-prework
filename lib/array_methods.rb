196a71731e984c5ca8de5a78192d4e31af0464fc
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-08-27 22:21:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,15 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  current_max = 0
+  array.length.times do |index|
+    if array[index] > current_max
+      current_max = array[index]
+    else
+      current_max = current_max
+    end
+  end
+  return current_max
 end
 
 def find_min_value(array)
